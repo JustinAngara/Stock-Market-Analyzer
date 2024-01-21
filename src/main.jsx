00cd4ba9e317c8893d29095bbd14eaa69faa6b4d
Justin Angara <justin.angara@gmail.com>
2024-01-21 17:24:15 -0600

    lesson 2 complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import SunAndMoon from "./SunAndMoon/SunAndMoon.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>Justin Angara</React.StrictMode>
+    <React.StrictMode>
+        Justin Angara
+        <SunAndMoon />
+    </React.StrictMode>
+
 );
