98f77987a33b7a311819fd11e9b924f1c0314aa1
Justin Angara <justin.angara@gmail.com>
2024-01-23 16:18:44 -0600

    L4 Complete

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -1,12 +1,16 @@
 
 import {Routes, Route, HashRouter} from "react-router-dom";
 
+
+
 //global styles
 import '@/Common/css/normalize.css';
 import '@/Common/css/global.scss';
 
 // layout
 import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
+import {MediaQueryProvider} from "@/Common/useMediaQuery.jsx";
+
 
 //components
 import Home from "./Home"
@@ -16,27 +20,29 @@ import Contact from "./Contact"
 // page componenents course work
 import CourseWorkLayout from "./CourseWork/CourseWorkLayout.jsx"
 import SunAndMoon from "./CourseWork/SunAndMoon/SunAndMoon.jsx"
-import Essays from "./CourseWork/Essays.jsx"
+import Essays from "./CourseWork/Essays/Essays.jsx"
 import ResponsiveDesign from "./CourseWork/ResponsiveDesign.jsx"
 const MyRoutes = () => {
     return (
     <HashRouter>
-        <Routes>
-            <Route element={<PagesLayout />}>
-                <Route element={<Home />} path=""/>
-                <Route element={<Staff />} path="staff"/>
-                <Route element={<Contact />} path="contact"/>
+        <MediaQueryProvider>
+            <Routes>
+                <Route element={<PagesLayout />}>
+                    <Route element={<Home />} path=""/>
+                    <Route element={<Staff />} path="staff"/>
+                    <Route element={<Contact />} path="contact"/>
 
-                <Route element={<CourseWorkLayout />} path="course-work">
-                    <Route element={<Essays />} path=""/>
-                    <Route element={<SunAndMoon />} path="sun-and-moon"/>
-                    <Route
-                        element={<ResponsiveDesign />}
-                        path="responsive"
-                    />
+                    <Route element={<CourseWorkLayout />} path="course-work">
+                        <Route element={<Essays />} path=""/>
+                        <Route element={<SunAndMoon />} path="sun-and-moon"/>
+                        <Route
+                            element={<ResponsiveDesign />}
+                            path="responsive"
+                        />
+                    </Route>
                 </Route>
-            </Route>
-        </Routes>
+            </Routes>
+        </MediaQueryProvider>
     </HashRouter>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Routes from "./Pages/Routes.jsx"
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-
+        
         <Routes />
     </React.StrictMode>
 
