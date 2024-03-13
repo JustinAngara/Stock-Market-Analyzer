0f4b93f4444f8cd8c7c78d00aef0fdcea1d39d86
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-03-13 15:22:18 +0000

    Changed footer

--- src/Common/PagesLayout/Footer.jsx
+++ src/Common/PagesLayout/Footer.jsx
@@ -12,8 +12,7 @@ const Footer = () => {
                     <SiteLogo />
                 </div>
                 <h2>
-                    CrossFit Decimate :: Peace, Love, Unity, Fitness, Chill, and Respect :: Colorado Springs, CO
-                </h2>
+                    StockPredicter :: Equity, Finance, and Investment :: San Diego, CA
                 <div className="copyright">
                     &copy; 2023. All rights reserved.
                 </div>
