57a13895698861a36dce4437308706b4c767a2a8
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-03-14 15:57:00 +0000

    Added Gradiant

--- src/Common/PagesLayout/PagesLayout.jsx
+++ src/Common/PagesLayout/PagesLayout.jsx
@@ -22,9 +22,21 @@ const PagesLayout = () => {
 
 export default PagesLayout;
 
+const color1 = "#343336";
+const color2 = "#B408A4";
+
 const PagesLayoutStyled = styled.div`
     main {
-        background-color: #fff;
+        // this is where the background color is
+        background: hsla(0, 0%, 59%, 1);
+
+        background: linear-gradient(90deg, hsla(0, 0%, 59%, 1) 0%, hsla(0, 0%, 54%, 1) 50%, hsla(0, 0%, 0%, 1) 100%);
+
+        background: -moz-linear-gradient(90deg, hsla(0, 0%, 59%, 1) 0%, hsla(0, 0%, 54%, 1) 50%, hsla(0, 0%, 0%, 1) 100%);
+
+        background: -webkit-linear-gradient(90deg, hsla(0, 0%, 59%, 1) 0%, hsla(0, 0%, 54%, 1) 50%, hsla(0, 0%, 0%, 1) 100%);
+
+        filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#969696", endColorstr="#898989", GradientType=1 );
         min-height: 600px;
         .Inset {
             padding: 50px 10px;
