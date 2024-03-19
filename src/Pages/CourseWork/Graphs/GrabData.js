065314f2b99f2d3396f39c8bb3d2e8ad2f499ce1
Justin Angara <justin.angara@gmail.com>
2024-03-18 19:06:28 -0500

    changed grab data

--- src/Pages/CourseWork/Graphs/GrabData.js
+++ src/Pages/CourseWork/Graphs/GrabData.js
@@ -1,8 +1,6 @@
 
 
 // use some apis
-// API: 84LJOAJ15Y72QTB8
-// full urL: https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=
 const apiKey = "84LJOAJ15Y72QTB8";
 let getDataCall = async () =>{
     let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${apiKey}`
