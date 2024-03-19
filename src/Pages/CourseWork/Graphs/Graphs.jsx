df955e1412774f4bcc3c634d3eb4831cbd47b04f
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-03-18 19:11:45 -0500

    Update Graphs.jsx

--- src/Pages/CourseWork/Graphs/Graphs.jsx
+++ src/Pages/CourseWork/Graphs/Graphs.jsx
@@ -1,28 +1,43 @@
 import styled from "styled-components";
-import PropTypes from "prop-types";
-import React from "react";
-import ReactPlayer from "react-player";
 
-const Graphs = () => {
+const Grid = () => {
+    const inventory = [
+        { title: "title 1", data: "data 1", insight: "insight 1" },
+        { title: "title 2", data: "data 2", insight: "insight 2" },
+        { title: "title 3", data: "data 3", insight: "insight 3" },
+        { title: "title 4", data: "data 4", insight: "insight 4" },
+        { title: "title 5", data: "data 5", insight: "insight 5" },
+    ];
+
     return (
-        <GraphsStyled>
-            <h1>Stock Market Explained</h1>
-            <ReactPlayer url="https://www.youtube.com/watch?v=ZCFkWDdmXG8" />
-
-            <br></br>
-
-            <h1>Ultimate Guide to Investing</h1>
-            <ReactPlayer url="https://www.youtube.com/watch?v=_MeZt4LBnCE&ab_channel=JamesJani" />
-
-        </GraphsStyled>
+        <GridStyled>
+            {inventory.map((item) => (
+                <GridItem key={item.title}>
+                    <h3>{item.title}</h3>
+                    <p>Data: {item.data}</p>
+                    <p>Insight: {item.insight}</p>
+                </GridItem>
+            ))}
+        </GridStyled>
     );
 };
 
-export default Graphs;
+export default Grid;
 
-// prop-types
-Graphs.propTypes = {
-    prop: PropTypes.array,
-};
+const GridStyled = styled.div`
+    display: grid;
+    grid-template-columns: repeat(
+        auto-fit,
+        minmax(250px, 1fr)
+    ); /* Responsive columns */
+    grid-gap: 10px; /* Spacing between items */
+    border: 1px solid #ff0000;
+    padding: 10px; /* Padding for content */
+`;
 
-const GraphsStyled = styled.div``;
+const GridItem = styled.div`
+    color: black;
+    background-color: #eee; /* Light background color */
+    padding: 10px; /* Padding for content */
+    border-radius: 5px; /* Rounded corners */
+`;
