0ea35739999d5d1d495aa33a89b57ee19a15ffa8
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-03-18 19:16:57 -0500

    Update GrabData.js

--- src/Pages/CourseWork/Graphs/GrabData.js
+++ src/Pages/CourseWork/Graphs/GrabData.js
@@ -1,9 +1,9 @@
 
 
 // use some apis
-// API: 84LJOAJ15Y72QTB8
+
 // full urL: https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=
-const apiKey = "84LJOAJ15Y72QTB8";
+const apiKey = "";
 let getDataCall = async () =>{
     let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${apiKey}`
     let response = fetch(url).then((res) => res.json());
