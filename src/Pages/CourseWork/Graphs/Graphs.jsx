901d847518be54eb1b2cf5584a2f307252035db8
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-04-02 15:52:30 +0000

    Added multi array graphs

--- src/Pages/CourseWork/Graphs/Graphs.jsx
+++ src/Pages/CourseWork/Graphs/Graphs.jsx
@@ -78,13 +78,14 @@ let parseApi = (data) => {
 
     let metaData = data["Meta Data"];
     let timeSeriesArr = data["Time Series (5min)"]; // obj
-
+    let symb = metaData["2. Symbol"];
 
     let timeSeriesArrKeys = Object.keys(timeSeriesArr);
     let timeSeriesArrValues = Object.values(timeSeriesArr);
     console.log(timeSeriesArrKeys);
     return (
         <div>
+            <h2>Symbol: {symb}</h2>
             <LineGraph data = {timeSeriesArrValues} keys = {timeSeriesArrKeys} />
             <TimeSeriesObj valuesArr={timeSeriesArrValues} />
         </div>
