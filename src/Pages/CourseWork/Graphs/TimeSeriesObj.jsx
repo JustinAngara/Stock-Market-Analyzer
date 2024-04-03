9d4c7170c60a3d0b0a0687c41a1e17eedb0c2139
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-04-03 15:18:02 +0000

    Added a toggle button to show open vs close costs table

--- src/Pages/CourseWork/Graphs/TimeSeriesObj.jsx
+++ src/Pages/CourseWork/Graphs/TimeSeriesObj.jsx
@@ -1,22 +1,40 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import React, { useState} from "react";
+
 let openCount = 0;
 let closeCount = 0;
+let iteration = 0;
 const TimeSeriesObj = ({ valuesArr }) => {
     console.log("values arr");
 
     const listItems = valuesArr.map(writeTables);
-
+    const [response, setResponse] = useState(0);
     return (
         <TimeSeriesObjStyled>
 
-            Open vs Close cost({openCount}, {closeCount})
-            {listItems}
+            Open vs Close cost({openCount}, {closeCount}) <br></br>
+            <button onClick={()=>{
+                console.log('inside onClick method')
+                if(iteration%2==0){
+                    setResponse(returnTables(listItems));
+                } else {
+                    setResponse(0);
+                }
+                iteration++;
+            }}>Show open vs close costs table</button>
+            {/* <button onClick={setResponse(()=>returnTables(listItems))}>Show open vs close costs table</button> */}
+            {/* {listItems} */}
+            {response}
             end
         </TimeSeriesObjStyled>
     );
 };
-
+let returnTables = (x) =>{
+    return <div>
+        {x}
+    </div>;
+}
 let writeTables = (e) => {
     let open = e["1. open"];
     let close = e["4. close"];
