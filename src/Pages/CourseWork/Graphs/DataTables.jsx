31c0c4d83a4d9436b92dafe402adebddd8ed8c76
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-04-04 20:26:04 +0000

    Connection with ag api

--- src/Pages/CourseWork/Graphs/DataTables.jsx
+++ src/Pages/CourseWork/Graphs/DataTables.jsx
@@ -1,14 +1,14 @@
-// import { PrimeReactProvider } from 'primereact/api';
-let DataTables = ({ Component, pageProps }) =>{
+import { PrimeReactProvider } from 'primereact/api';
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+
+
+let DataTables = () =>{
+
 
     return (
         <div>
-
-        {/* <PrimeReactProvider>
-            <Component {...pageProps} />
-        </PrimeReactProvider> */}
-
-
 
         </div>
 
