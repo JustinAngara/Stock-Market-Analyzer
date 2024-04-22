76701b495cfe9f5aa1b717d88b0a670e5801981d
Justin Angara <78995665+JustinAngara@users.noreply.github.com>
2024-04-22 15:33:40 +0000

    Added more stuff

---
+++ src/Pages/CourseWork/Graphs/PredictionAlgorithm.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const PredictionAlgorithm = () => {
+    return <PredictionAlgorithmStyled>
+        <h1>Predicted Best Algorithm Stock</h1>
+        <div>
+            n-stocks:
+        </div>
+    </PredictionAlgorithmStyled>;
+}
+
+export default PredictionAlgorithm;
+
+
+const PredictionAlgorithmStyled = styled.div`
+
+`;
