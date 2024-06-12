d5d1500b438a7356127560cc7ab4380e9816e5b2
Justin Angara <justin.angara@gmail.com>
2024-06-12 02:46:52 -0500

    Starting news api

--- src/Pages/CourseWork/Graphs/GrabNews.jsx
+++ src/Pages/CourseWork/Graphs/GrabNews.jsx
@@ -1,5 +1,21 @@
-let GrabNews = (stringStock) =>{
-    return "now in the getNewsApiEval"
-}
+// news api a57bbac925b34dfe9fab71e17d542065
+let API_KEY = "a57bbac925b34dfe9fab71e17d542065";
+let GrabNews = (stringStock) => {
+    getData("tesla");
+    return <div>hello</div>;
+};
 
+let getData = async (stringStock) => {
+    const response = await fetch(
+        `https://newsapi.org/v2/everything?q=${stringStock}&from=2024-05-12&sortBy=publishedAt&apiKey=${API_KEY}`
+    );
+    const movies = await response.json();
+    console.log(movies);
+
+    for (let i = 0; i < movies.articles.length; i++) {
+        console.log(""+movies.articles[i].description);
+    }
+
+
+};
 export default GrabNews;
--- src/Pages/CourseWork/Graphs/PredictionAlgorithm.jsx
+++ src/Pages/CourseWork/Graphs/PredictionAlgorithm.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
-import getTestData from './getTestData';
-import GrabNews from "./GrabNews"
+import styled from "styled-components";
+import getTestData from "./getTestData";
+import GrabNews from "./GrabNews";
 
 let data = getTestData(true);
 let globalSDArr; // global sd array
@@ -9,81 +9,81 @@ const PredictionAlgorithm = () => {
     globalSDArr = populateSDArray(data);
     globalPearsonArr = populatePearsonArr(data);
 
-    return <PredictionAlgorithmStyled>
-        <h1>Predicted Best Algorithm Stock</h1>
-        <div>
-            <div className="bold">
-                Recommended Stock: {getRecStock(data)}  <br></br>
+    return (
+        <PredictionAlgorithmStyled>
+            <h1>Predicted Best Algorithm Stock</h1>
+            <div>
+                <div className="bold">
+                    Recommended Stock: {getRecStock(data)} <br></br>
+                </div>
+                <strong>Most Stable</strong> (low SD): {lowestVar(data)}
+                <br></br>
+                <strong>List of stocks</strong>: [
+                {grabStocks(data, 0).toString()}]<br></br>
+                <strong>n-stocks</strong>: {data.length} <br></br>
+                {/* <strong>
+                    example fricking stuff: <GrabNews />
+                </strong> */}
+                <GrabNews />
             </div>
-
-            <strong>Most Stable</strong> (low SD): {lowestVar(data)}<br></br>
-
-            <strong>List of stocks</strong>: [{grabStocks(data, 0).toString()}]<br></br>
-
-            <strong>n-stocks</strong>: {data.length} <br></br>
-        </div>
-    </PredictionAlgorithmStyled>;
-}
+        </PredictionAlgorithmStyled>
+    );
+};
 
 export default PredictionAlgorithm;
 
-let getRecStock = (d) =>{
-
+let getRecStock = (d) => {
     let t = [];
-    for(let i =0; i<d.length;i++){
-        console.log('now in getRecStock');
+    for (let i = 0; i < d.length; i++) {
+        console.log("now in getRecStock");
         t.push(calculateStockValue(d[i], i)); // this method returns an eval
     }
-    console.log('in get rec stock look here');
+    console.log("in get rec stock look here");
     console.log(t);
     let max = -9999;
     let index = 0;
-    for(let i = 0; i<t.length;i++){
-
-        if(max<t[i]){
+    for (let i = 0; i < t.length; i++) {
+        if (max < t[i]) {
             index = i;
-            max = t[i]
+            max = t[i];
         }
     }
 
-    console.log('this is d in rec, i:'+index);
+    console.log("this is d in rec, i:" + index);
     console.log();
     console.log(d[index]);
-    return d[index]["Meta Data"]["2. Symbol"]
-}
-
+    return d[index]["Meta Data"]["2. Symbol"];
+};
 
 /**
 let grabPearsonsCoefficient = (stock, z, mu) =>{ // return stock's pearson coefficient
  *
  */
 let populatePearsonArr = (d) => {
-    console.log('in populate pearson arr');
+    console.log("in populate pearson arr");
     console.log(d);
     let t = [];
-    for(let i = 0; i<d.length;i++){
+    for (let i = 0; i < d.length; i++) {
         t.push(grabPearsonsCoefficient(d[i], i, getMean(d[i])));
     }
-    console.log('in total populate pearson arr');
+    console.log("in total populate pearson arr");
     console.log(t);
     return t;
-}
+};
 
-let populateSDArray = (d) =>{
-    console.log('in populate sd arrayy');
+let populateSDArray = (d) => {
+    console.log("in populate sd arrayy");
     console.log(d);
     let t = [];
 
-    for(let i = 0; i < d.length; i++){
+    for (let i = 0; i < d.length; i++) {
         t.push(getSD(d[i]));
     }
     t = t.sort();
-    console.log('this is t');
+    console.log("this is t");
     console.log(t);
     return t;
-}
-
-
+};
 
 /**
  * # of lower bound outliers, val: .4
@@ -91,7 +91,7 @@ let populateSDArray = (d) =>{
  * pearson skewness gets exponentially greater: .1
  * news article parser to determine positive or negative based on eVal, val: .3
  * */
-let calculateStockValue = (z, i) =>{
+let calculateStockValue = (z, i) => {
     // this is an array of each object that shows open, close, volume, etc
     let tsv = Object.values(z["Time Series (5min)"]);
 
@@ -99,159 +99,148 @@ let calculateStockValue = (z, i) =>{
     // console.log(`now in get rec stock ${GrabNews()}`);
     let sd = getSD(z);
     let mu = 0;
-    console.log('calculate stock value method');
+    console.log("calculate stock value method");
     console.log(sd);
 
     // if sd < sd[sd.length*.75] && sd > sd[sd.length*.25]
-    for(let i =0 ; i < tsv.length; i++){
+    for (let i = 0; i < tsv.length; i++) {
         // tsv[i] is the individual 5 minute intervals at stock index i
         console.log(tsv[i]);
-        mu += 1*(tsv[i]["1. open"]);
-
-
-
+        mu += 1 * tsv[i]["1. open"];
     }
-    mu/=10; // calculates mean of the price
+    mu /= 10; // calculates mean of the price
 
     let pearson = grabPearsonsCoefficient(z, i, mu);
-    let numOfOutliers = getNumOfOutliers(z,i,mu);
-    console.log('this is pearsons coefficient');
+    let numOfOutliers = getNumOfOutliers(z, i, mu);
+    console.log("this is pearsons coefficient");
     console.log(pearson);
-
 
     // temp variable for sorted global SD array for proper proportions
     let tssd = globalSDArr.sort();
 
     // temp variable for sorted global pearson array
     let tpa = globalPearsonArr.sort();
-    let mult = .75;
+    let mult = 0.75;
 
     // checks within the sd of being within the middle 50%
-    if(sd>=tssd[tssd.length*.25] && sd<=tssd[tssd.length*.75]){
+    if (sd >= tssd[tssd.length * 0.25] && sd <= tssd[tssd.length * 0.75]) {
         // nice multipler
         mult = 1.25;
     }
 
     // checks if the pearson coefficient is within the 75th percentile
-    if(pearson>=tpa[tpa.length*.75]){
-        mult+=.5
+    if (pearson >= tpa[tpa.length * 0.75]) {
+        mult += 0.5;
     }
 
-    mult += (.1*numOfOutliers)
+    mult += 0.1 * numOfOutliers;
 
-    console.log('inside the global pearson arr');
+    console.log("inside the global pearson arr");
     console.log(globalPearsonArr);
 
     // next check if the pearson coefficient is within
     return mult;
-}
+};
 
 // d[i], raw, index, mean
-let getNumOfOutliers = (stock,z,mu) =>{
-    stock = stock['Time Series (5min)']
-    let values =  Object.values(stock);
-    values = values.map((e)=>{
-        return 1*e['1. open']
-    })
+let getNumOfOutliers = (stock, z, mu) => {
+    stock = stock["Time Series (5min)"];
+    let values = Object.values(stock);
+    values = values.map((e) => {
+        return 1 * e["1. open"];
+    });
     values = values.sort();
 
-    let iqr = (1*values[74]) - (1*values[24])
-    iqr *=1.5;
-    console.log('numofoutliersiqr');
+    let iqr = 1 * values[74] - 1 * values[24];
+    iqr *= 1.5;
+    console.log("numofoutliersiqr");
     console.log(iqr);
-    let lowerbound = 1*values[24]-iqr;
+    let lowerbound = 1 * values[24] - iqr;
 
-    for(let i = 0; i<values.length;i++){
-        if(values[i]>lowerbound){
-            console.log('lowerbound counted'+i+1);
-            return i+1;// essentially since its sorted, it looks for the max values after values[i]
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] > lowerbound) {
+            console.log("lowerbound counted" + i + 1);
+            return i + 1; // essentially since its sorted, it looks for the max values after values[i]
         }
     }
     return 0;
-}
+};
 
 // stock is d[i], z:index, mu
-let grabPearsonsCoefficient = (stock, z, mu) =>{ // return stock's pearson coefficient
+let grabPearsonsCoefficient = (stock, z, mu) => {
+    // return stock's pearson coefficient
     // pearsons coefficient - (3(mu-med))/sd
-    stock = stock['Time Series (5min)']
-    mu = 1*mu;
+    stock = stock["Time Series (5min)"];
+    mu = 1 * mu;
 
     let sd = globalSDArr[z];
-    let values =  Object.values(stock);
-    let median = Math.floor(values.length/2)-1; // returns index
-    median = values[median]["1. open"] // enters the values array then median index
+    let values = Object.values(stock);
+    let median = Math.floor(values.length / 2) - 1; // returns index
+    median = values[median]["1. open"]; // enters the values array then median index
     console.log(`
         mu: ${mu}
         med: ${median}
         sd: ${sd}
     `);
-    return (3*(mu-median))/sd;
+    return (3 * (mu - median)) / sd;
+};
 
-}
-
-let grabStocks = (d, z) =>{
-    if(z!==0){
-        return d[z]["Meta Data"]["2. Symbol"]
+let grabStocks = (d, z) => {
+    if (z !== 0) {
+        return d[z]["Meta Data"]["2. Symbol"];
     }
     let t = [];
-    for(let i =0; i<d.length;i++){
-        console.log('this is symbol');
+    for (let i = 0; i < d.length; i++) {
+        console.log("this is symbol");
 
         console.log(d[i]["Meta Data"]["2. Symbol"]);
         t.push(d[i]["Meta Data"]["2. Symbol"]);
-
     }
 
     return t.toString();
-
-}
-let lowestVar = (d) =>{
+};
+let lowestVar = (d) => {
     let minVar = 99999;
     let tinkerMin = "";
     let temp;
-    for(let i = 0; i<d.length; i++){
-        temp = getSD(d[i])
-        if(temp < minVar){
+    for (let i = 0; i < d.length; i++) {
+        temp = getSD(d[i]);
+        if (temp < minVar) {
             console.log(`this is temp: ${temp}`);
             minVar = temp;
-            tinkerMin = grabStocks(data,i)
+            tinkerMin = grabStocks(data, i);
         }
     }
-    return `Tinker: ${tinkerMin}, SD: (${minVar})`
-}
+    return `Tinker: ${tinkerMin}, SD: (${minVar})`;
+};
 
-let invNorm = (x) =>{
+let invNorm = (x) => {};
 
-}
-
-
-let getMean = (d) =>{
-    console.log('in mean method');
+let getMean = (d) => {
+    console.log("in mean method");
 
     d = d["Time Series (5min)"];
     d = Object.values(d);
     let z = 0;
-    for(let i = 0; i<d.length;i++){
-        z+=(1*d[i]['1. open'])
-
+    for (let i = 0; i < d.length; i++) {
+        z += 1 * d[i]["1. open"];
     }
-    console.log('get mean method');
+    console.log("get mean method");
     console.log(z);
     // const n = open.length;
     // let open = d.map((e) => Number(e["1. open"]));
     // return open.reduce((a, b) => a + b) / n;
-    return z/d.length;
-}
+    return z / d.length;
+};
 let getSD = (d) => {
-    console.log('getSD method');
+    console.log("getSD method");
 
     d = d["Time Series (5min)"];
     d = Object.values(d);
 
+    let open = d.map((e) => Number(e["1. open"]));
 
-    let  open = d.map((e) => Number(e["1. open"]));
-
-    console.log('in d');
+    console.log("in d");
     console.log(open);
     // console.log(open);
 
@@ -259,15 +248,14 @@ let getSD = (d) => {
     const mean = open.reduce((a, b) => a + b) / n;
     return Math.sqrt(
         open.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
-
     );
-}
+};
 
 const PredictionAlgorithmStyled = styled.div`
-    .displayStocks{
+    .displayStocks {
         border: 1px;
     }
-    .bold{
+    .bold {
         font-size: 20px;
         font-weight: bold;
     }
