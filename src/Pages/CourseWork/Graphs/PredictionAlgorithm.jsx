c98a04c1886445758979f151f8e3d8af204c6d79
Justin Angara <justin.angara@gmail.com>
2024-06-12 03:11:34 -0500

    .

--- src/Pages/CourseWork/Graphs/PredictionAlgorithm.jsx
+++ src/Pages/CourseWork/Graphs/PredictionAlgorithm.jsx
@@ -24,7 +24,7 @@ const PredictionAlgorithm = () => {
                 {/* <strong>
                     example fricking stuff: <GrabNews />
                 </strong> */}
-                Change: <GrabNews />
+                Changes: <GrabNews />
             </div>
         </PredictionAlgorithmStyled>
     );
